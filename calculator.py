2256ac3beaf6861e22ca437229036abc4fd1071b
cmd-Enock <enockboateng122@gmail.com>
2025-08-10 08:36:03 +0000

    Week 1 assignment

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+
+sum_result = num1 + num2
+
+difference_result = num1 - num2
+
+product_result = num1 * num2
+
+mean_result = num2 / num1
+percentage_results = num2 % num1
+
+print("Results:")
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Produ ct: {product_result}")  
+print(f"mean: {mean_result}")
+print(f"percentage: {percentage_results}")
